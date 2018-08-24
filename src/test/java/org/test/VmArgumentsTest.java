83ab42a6ca72839ab278d1e882e14990ec29d7e7
tzimanyi <tibor.zimanyi@gmail.com>
2018-08-24 11:12:24 +0200

    Rewording in a comment

--- src/test/java/org/test/VmArgumentsTest.java
+++ src/test/java/org/test/VmArgumentsTest.java
@@ -6,8 +6,8 @@ public class VmArgumentsTest {
 
     @Test
     public void testVmArguments() {
-        // Just set a VM option -Xmx in Run configuration and you will see, this output is still the same
-        // because of surefire settings from parent Maven project.
+        // Just set a VM option -Xmx in Run configuration to something other that 1024m and you will see,
+        // this output is still the same because of Surefire settings from parent Maven project.
         // When I run this in Eclipse with different -Xmx argument in Run configuration, it changes.
         System.out.println(Runtime.getRuntime().maxMemory());
     }
